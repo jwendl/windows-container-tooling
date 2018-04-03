a23532d595b6a592091249f064a83a31fb1f06d3
Justin Wendlandt <jwendl@hotmail.com>
2018-04-03 10:25:34 -0700

    Initial Commit.

---
+++ src/ServiceProcessWatcher/ServiceProcessWatcher.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceProcessWatcher.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
